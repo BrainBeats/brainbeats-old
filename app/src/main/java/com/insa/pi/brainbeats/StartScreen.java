5c456d0f4887194f5744737ab966ddede5926cf9
samutamm <samutamm@gmail.com>
2016-05-09 11:45:06 +0200

    Start screen.

--- app/src/main/java/com/insa/pi/brainbeats/StartScreen.java
+++ app/src/main/java/com/insa/pi/brainbeats/StartScreen.java
@@ -4,14 +4,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class StartScreen extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        listView = (ListView) findViewById(R.id.playlist_list);
+
+        String[] values = new String[] {
+                "Playlist 1",
+                "Playlist 2"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // ICI on va mettre un peu de logique
+            }
+
+        });
     }
 
     @Override
